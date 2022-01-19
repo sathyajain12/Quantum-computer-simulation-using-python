f6243fcda7c1e9a233a66530b3e9c3600f75c58a
sathyajain12 <50107054+sathyajain12@users.noreply.github.com>
2022-01-19 14:18:56 +0530

    Create Quantum_circuit.py

---
+++ Quantum_circuit.py
@@ -0,0 +1,4 @@
+circuit = q.QuantumCircuit(2,2)  # 2 qubits, 2 classical bits 
+circuit.x(0) # "x" is a "NOT" gate. It flips the value. Starting value is a 0, so this flips to a 1. 
+circuit.cx(0, 1) #"CNOT", controlled not, Flips 2nd qubit's value if first qubit is 1
+circuit.measure([0,1], [0,1])  # ([qbitregister], [classicalbitregister]) Measure qubit 0 and 1 to classical bits 0 and 1
